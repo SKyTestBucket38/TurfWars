f678552853f27c4c552bd503f58e8e41c8ce66c1
notacoderr <38270548+notacoderr@users.noreply.github.com>
2019-08-20 23:01:37 +0400

    Update Task.php

--- src/TurfWars/Task.php
+++ src/TurfWars/Task.php
@@ -3,18 +3,12 @@
 namespace TurfWars;
 
 use pocketmine\scheduler\Task;
-use pocketmine\Server; 
-use pocketmine\level\Level;
-use pocketmine\math\Vector3;
-use pocketmine\tile\Sign;
-use pocketmine\utils\TextFormat as F;
+use pocketmine\Server;
 
 class Task extends Task {
 
 	public function __construct(Main $plugin){
-		parent::__construct($plugin);
 		$this->plugin = $plugin;
-
 	}
 
 
